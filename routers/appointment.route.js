9ed96662b989918809a53d20becb369081051142
nguyentuan <chichi@MacBook-Pro-cua-nguyentuan.local>
2026-05-07 22:41:13 +0700

    chore: remove stray duplicated backend and frontend files from root

--- routers/appointment.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { registerAppointment } from "../controllers/appointment.controller.js";
-
-const router = express.Router();
-
-// Endpoint đăng ký khám bệnh online
-router.post("/register", registerAppointment);
-
-export default router;
